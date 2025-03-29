6a7caa376756db2cd64dd264e68e7e1dc42631c1
wendellu18 <161165357+wendellu18@users.noreply.github.com>
2025-03-29 14:08:06 -0300

    Update historia.py
    alteração por wendel lucas 3b

--- historia.py
+++ historia.py
@@ -8,7 +8,6 @@ sus1 = input('Apelido do suspeito 1: ')
 sus2 = input ('Apelido do suspeito 2: ')
 print ('''
        O aprendiz {} estava em seu 4° ano de treinamento com seu mestre {}, o Mestre Kid tinha um exelente dominio na arte de dominação do gelo!
-       todos os dias era desafiado por varios especialistas renomados.. porém um certo dia ele foi morto
-       pelas costas quando o seu aprendiz de codinome {} acabara de completar {}. 
-       os principais suspeitos {} e {} (Tinham bastante inveja do Mestre {}) seu aprendiz {} sai em caçada para tentar vingar seu mestre {}... '''
+       todos os dias era desafiado por varios especialistas renomados.. por isso ele enviou seu aorendiz de codinome {} que acabara de completar {} em uma missão para caçar os suspeitos que o ameaçam constantemente:  
+        {} e {} (Tinham bastante inveja do Mestre {}) seu aprendiz {} sai em caçada para realizar a missão que seu mestre {} o designou ... '''
        .format(nome,mestre, nome_he, idade, sus1, sus2, mestre, nome_he, mestre))
