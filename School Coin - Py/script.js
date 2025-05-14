b792584444186403ef727e9cc3fca7725a199c43
umjotta <heliojuniorsly@gmail.com>
2025-05-14 15:45:10 -0300

    Delete School Coin - Py directory

--- School Coin - Py/script.js
+++
@@ -1,7 +0,0 @@
-function redirecionar(tipo) {
-  if (tipo === "aluno") {
-    window.location.href = "telaAluno.html"; // Próxima tela do aluno
-  } else if (tipo === "professor") {
-    window.location.href = "telaProfessor.html"; // Próxima tela do professor
-  }
-}
